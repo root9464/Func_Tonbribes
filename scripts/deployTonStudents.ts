ddf44ee32ef9508b8714b59f387e104b835a1a30
io <senpai9464@yandex.ru>
2024-11-11 12:53:48 +0000

    так ну ск билдится уже заебись

--- scripts/deployTonStudents.ts
+++ scripts/deployTonStudents.ts
@@ -1,13 +1,21 @@
-import { toNano } from '@ton/core';
-import { TonStudents } from '../wrappers/TonStudents';
 import { compile, NetworkProvider } from '@ton/blueprint';
+import { Address, toNano } from '@ton/core';
+import { TonStudents } from '../wrappers/TonStudents';
 
 export async function run(provider: NetworkProvider) {
-    const tonStudents = provider.open(TonStudents.createFromConfig({}, await compile('TonStudents')));
+  const owner: Address = Address.parse('0QAYRw04JzUo1IEK6TL6vKfos66gsdN6vUFfJeA3OOjOfDPG');
+  const tonStudents = provider.open(
+    TonStudents.createFromConfig(
+      {
+        admin_address: owner,
+      },
+      await compile('TonStudents'),
+    ),
+  );
 
-    await tonStudents.sendDeploy(provider.sender(), toNano('0.05'));
+  await tonStudents.sendDeploy(provider.sender(), toNano('0.05'));
 
-    await provider.waitForDeploy(tonStudents.address);
+  await provider.waitForDeploy(tonStudents.address);
 
-    // run methods on `tonStudents`
+  // run methods on `tonStudents`
 }
