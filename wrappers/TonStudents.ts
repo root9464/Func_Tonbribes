e4fba814fe1cbf5454b98bf310fb287f5bd5baa6
io <senpai9464@yandex.ru>
2024-11-11 13:53:15 +0000

    геттер робит

--- wrappers/TonStudents.ts
+++ wrappers/TonStudents.ts
@@ -1,29 +1,34 @@
 import { Address, beginCell, Cell, Contract, contractAddress, ContractProvider, Sender, SendMode } from '@ton/core';
 
-export type TonStudentsConfig = {};
+export type TonStudentsConfig = {
+  admin_address: Address;
+};
 
 export function tonStudentsConfigToCell(config: TonStudentsConfig): Cell {
-    return beginCell().endCell();
+  return beginCell().storeAddress(config.admin_address).endCell();
 }
 
 export class TonStudents implements Contract {
-    constructor(readonly address: Address, readonly init?: { code: Cell; data: Cell }) {}
+  constructor(
+    readonly address: Address,
+    readonly init?: { code: Cell; data: Cell },
+  ) {}
 
-    static createFromAddress(address: Address) {
-        return new TonStudents(address);
-    }
+  static createFromAddress(address: Address) {
+    return new TonStudents(address);
+  }
 
-    static createFromConfig(config: TonStudentsConfig, code: Cell, workchain = 0) {
-        const data = tonStudentsConfigToCell(config);
-        const init = { code, data };
-        return new TonStudents(contractAddress(workchain, init), init);
-    }
+  static createFromConfig(config: TonStudentsConfig, code: Cell, workchain = 0) {
+    const data = tonStudentsConfigToCell(config);
+    const init = { code, data };
+    return new TonStudents(contractAddress(workchain, init), init);
+  }
 
-    async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
-        await provider.internal(via, {
-            value,
-            sendMode: SendMode.PAY_GAS_SEPARATELY,
-            body: beginCell().endCell(),
-        });
-    }
+  async sendDeploy(provider: ContractProvider, via: Sender, value: bigint) {
+    await provider.internal(via, {
+      value,
+      sendMode: SendMode.PAY_GAS_SEPARATELY,
+      body: beginCell().endCell(),
+    });
+  }
 }
